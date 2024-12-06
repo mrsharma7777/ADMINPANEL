89ed06c5244ad71450026859575767d4714456e6
mrsharma7777 <127834732+mrsharma7777@users.noreply.github.com>
2024-12-06 17:26:47 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -621,8 +621,9 @@ app.get('/usertotal', async (req, res) => {
 
 // Server listening on port 8080
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 // to display all transactions//
